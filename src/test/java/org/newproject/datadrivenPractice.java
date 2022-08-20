cb8d7925ce2f64c1f1d8c2e6b539dc7e560d0d1e
SridharanLoganathan <suyandharan25@gmail.com>
2022-08-20 14:19:23 +0530

    git test

---
+++ src/test/java/org/newproject/datadrivenPractice.java
@@ -0,0 +1,9 @@
+package org.newproject;
+
+import java.io.File;
+
+public class datadrivenPractice {
+
+	File f=new File("C:\\Users\\sridh\\eclipse-workspace\\FrameworkPractice\\DatadrivenXLsheet\\Book1.xlsx");
+	
+}
